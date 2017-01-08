d833d69c53f83b715f269158db880c07cdd0b8de
ASzoltysek <A@ania.home>
2017-01-08 20:39:07 +0100

    minor changes

--- src/com/adventOfCode/annieq/day4/Part1.java
+++ src/com/adventOfCode/annieq/day4/Part1.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
