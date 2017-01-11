1d8bdcaccefdaebaabdec2007ddb3938f03dd355
ASzoltysek <ASzoltysek@NBASzoltysek>
2017-01-11 15:31:46 +0100

    Reverting to correct version: day1/Part2.java

--- src/com/adventOfCode/annieq/day1/Part2.java
+++ src/com/adventOfCode/annieq/day1/Part2.java
@@ -7,16 +7,17 @@ import java.util.List;
 import com.adventOfCode.annieq.utilities.InputImport;
 
 public class Part2 {
+        
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		InputImport in = new InputImport("data//day1.txt");
 		List<String> data = in.getSingleLineData(", ");
-		int[] coords = new int[] {0, 0};
+		Coords coords = new Coords(0, 0);
 		Direction dir = Direction.NORTH;
-		int counter = 1;
-		List<int[]> visited = new ArrayList<int[]>();
-		int[] result = new int[2];
+		List<Coords> visited = new ArrayList<Coords>();
 		int stepLength = 0;
+		boolean intersection = false;
+		
+		//visited.add(new Coords(2,0));
 		
 		for (String step : data) {
 			// get direction
@@ -31,26 +32,72 @@ public class Part2 {
 			stepLength = Integer.parseInt(step.substring(1));
 			switch (dir) {
 			case NORTH:
-				coords[1] += stepLength;
 				for (int i=0; i < stepLength; ++i) {
-					//visited.add(coords[0])
+				    Coords tmp = new Coords(coords.x(), coords.y() + i);
+					if (visited.contains(tmp)) {
+					    intersection = true;
+					    coords = tmp;
+					    break;
+					} else {
+					    visited.add(tmp);					    
+					}
 				}
+				if (!intersection)
+				    coords.setY(coords.y() + stepLength);
 				break;
 			case SOUTH:
-				coords[1] -= stepLength;
+                for (int i=0; i < stepLength; ++i) {
+                    Coords tmp = new Coords(coords.x(), coords.y() - i);
+                    if (visited.contains(tmp)) {
+                        intersection = true;
+                        coords = tmp;
+                        break;
+                    } else {
+                        visited.add(tmp);                       
+                    }                   
+                }
+                if (!intersection)
+                    coords.setY(coords.y() - stepLength);
 				break;
 			case EAST:
-				coords[0] += stepLength;
+                for (int i=0; i < stepLength; ++i) {
+                    Coords tmp = new Coords(coords.x() + i, coords.y());
+                    if (visited.contains(tmp)) {
+                        intersection = true;
+                        coords = tmp;
+                        break;
+                    } else {
+                        visited.add(tmp);                       
+                    }                                      
+                }
+                if (!intersection)
+                    coords.setX(coords.x() + stepLength);
 				break;
 			case WEST:
-				coords[0] -= stepLength;
+                for (int i=0; i < stepLength; ++i) {
+                    Coords tmp = new Coords(coords.x() - i, coords.y());
+                    if (visited.contains(tmp)) {
+                        intersection = true;
+                        coords = tmp;
+                        break;
+                    } else {
+                        visited.add(tmp);                       
+                    }                                      
+                }
+                if (!intersection)
+                    coords.setX(coords.x() - stepLength);
 				break;
 			}
-			System.out.println(counter + ". Step: " + step);
-			System.out.println("Direction: " + dir);
-			System.out.println("Coords: " + coords[0] + ", " + coords[1]);
-			++counter;
+//			System.out.println(counter + ". Step: " + step);
+//			System.out.println("Direction: " + dir);
+//			System.out.println("Coords: " + coords[0] + ", " + coords[1]);
+//			++counter;
+			if (intersection) {
+			    break;
+			}
 		}
-		System.out.println("Result: " + result[0] + ", " + result[1]);
+		System.out.println("Result: " + coords.x() + ", " + coords.y());
+		System.out.println(Math.addExact(Math.abs(coords.x()), Math.abs(coords.y())));
 	}
+	
 }
